5c3110b67f4290161bf193de3afe1cda0bed7cd1
harshit Mishra <miananya2@gmail.com>
2024-10-26 03:06:08 +0530

    server creation API

--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import {
   Dialog,
@@ -24,9 +25,11 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "../FileUpload";
+import { useRouter } from "next/navigation";
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -43,7 +46,14 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof modalSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isMounted) {
