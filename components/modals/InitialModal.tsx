5089cf5cada0279768e178a0b49f017dbc54dba7
harshit Mishra <miananya2@gmail.com>
2024-10-26 02:37:37 +0530

    uploadthing image upload

--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -23,6 +23,7 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "../FileUpload";
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -65,7 +66,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
