332afffd093827a60a55975bc97560b6e661f1cc
harshit Mishra <miananya2@gmail.com>
2024-10-27 05:02:39 +0530

    manage members

--- provider/ModalProvider.tsx
+++ provider/ModalProvider.tsx
@@ -3,6 +3,7 @@
 import { CreateServerModal } from "@/components/modals/CreateServerModal";
 import { EditServerModal } from "@/components/modals/EditServerModal";
 import { InviteModal } from "@/components/modals/InviteModal";
+import { MembersModal } from "@/components/modals/MembersModal";
 import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
@@ -21,6 +22,7 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
