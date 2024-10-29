c4e51a75f3af284ed2b179396d1b40819e817f87
harshit Mishra <miananya2@gmail.com>
2024-10-29 09:47:03 +0530

    edit and delete channel

--- provider/ModalProvider.tsx
+++ provider/ModalProvider.tsx
@@ -2,7 +2,9 @@
 
 import { CreateChannelModal } from "@/components/modals/CreateChannelModal";
 import { CreateServerModal } from "@/components/modals/CreateServerModal";
+import { DeleteChannelModal } from "@/components/modals/DeleteChannelModal";
 import { DeleteServerModal } from "@/components/modals/DeleteServerModal";
+import { EditChannelModal } from "@/components/modals/EditChannelModal";
 import { EditServerModal } from "@/components/modals/EditServerModal";
 import { InviteModal } from "@/components/modals/InviteModal";
 import { LeaveServerModal } from "@/components/modals/LeaveServerModal";
@@ -29,6 +31,8 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
