86add906df5b534c53b667f3824e4d975f921914
harshit Mishra <miananya2@gmail.com>
2024-10-29 23:37:35 +0530

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,46 @@
-export default function ChannelIdPage() {
-  return <div>Channel Id Page</div>;
+import { ChatHeader } from "@/components/chat/ChatHeader";
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 }
