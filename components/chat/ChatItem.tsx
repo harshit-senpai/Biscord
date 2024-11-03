cb3576dfb8a02247160ce95105a5dad95f2daac4
harshit Mishra <miananya2@gmail.com>
2024-11-03 20:55:31 +0530

    delete message

--- components/chat/ChatItem.tsx
+++ components/chat/ChatItem.tsx
@@ -15,6 +15,7 @@ import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import axios from "axios";
+import { useModal } from "@/hooks/useModalStore";
 
 interface ChatItemProps {
   id: string;
@@ -56,7 +57,7 @@ export const ChatItem = ({
   const fileType = fileUrl?.split(".").pop();
 
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+  const { onOpen } = useModal();
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -219,7 +220,15 @@ export const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label={"Delete"}>
-            <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-800 dark:hover:text-zinc-300 transition" />
+            <Trash
+              onClick={() =>
+                onOpen("deleteMessage", {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+              className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-800 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
